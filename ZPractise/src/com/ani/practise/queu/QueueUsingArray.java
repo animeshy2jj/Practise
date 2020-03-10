53cb5cec501e33e0bd505888a1604488612cbcb9
Animesh Kumar <animkumar@manh.com>
2020-03-10 18:38:15 +0530

    queue

---
+++ ZPractise/src/com/ani/practise/queu/QueueUsingArray.java
@@ -0,0 +1,12 @@
+package com.ani.practise.queu;
+
+public class QueueUsingArray<T> {
+    Object[] array;
+    int current = 0;
+
+    void createQueue(int size) {
+	if (current == 0) {
+
+	}
+    }
+}
--- ZPractise/src/com/ani/practise/stack/StackLinkedListMain.java
+++ ZPractise/src/com/ani/practise/stack/StackLinkedListMain.java
@@ -3,18 +3,14 @@ package com.ani.practise.stack;
 public class StackLinkedListMain {
 
     public static void main(String[] args) {
-	StackUsingLinkedList<String> st = new StackUsingLinkedList<String>();
-	st.push("A");
-	st.push("B");
-	st.push("C");
-	st.push("D");
-	st.push("E");
-	st.push("F");
+	StackUsingLinkedList<Integer> st = new StackUsingLinkedList<Integer>();
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.push(4);
+	st.push(5);
+	st.push(6);
 	st.printStack();
-	System.out.println("PEEK :" + st.peek());
-	System.out.println("POPPED :" + st.pop());
-	System.out.println("PEEK :" + st.peek());
-	System.out.println("POPPED :" + st.pop());
 	System.out.println("PEEK :" + st.peek());
 	System.out.println("POPPED :" + st.pop());
 	System.out.println("PEEK :" + st.peek());
